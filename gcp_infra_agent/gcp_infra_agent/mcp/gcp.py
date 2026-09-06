62f14eae1c86448b3d176fda504855f6955e1bb6
Shashank Palla <shashankpalla44@gmail.com>
2026-09-06 16:08:34 +0530

    fix(infra-agent): removed id token for gcp mcp auth and added access token

--- gcp_infra_agent/gcp_infra_agent/mcp/gcp.py
+++ gcp_infra_agent/gcp_infra_agent/mcp/gcp.py
@@ -1,13 +1,11 @@
 import google.auth
 from google.auth.transport.requests import Request
-from google.oauth2 import id_token
 
 from google.adk.tools.mcp_tool.mcp_toolset import McpToolset
 from google.adk.tools.mcp_tool.mcp_session_manager import StreamableHTTPConnectionParams
 
 
 GCP_MCP_URL = "https://run.googleapis.com/mcp"
-
 
 READ_ONLY_TOOL_NAMES = [
     "get_service",
@@ -18,14 +16,11 @@ def get_adc_token() -> str:
     credentials, _ = google.auth.default()
     auth_req = Request()
 
-    return id_token.fetch_id_token(
-        auth_req,
-        self.target_audience_url,
-    )
+    return credentials.token
 
 def create_gcp_mcp_toolset() -> McpToolset:
     token = get_adc_token()
-    
+
     return McpToolset(
         connection_params=StreamableHTTPConnectionParams(
             url=GCP_MCP_URL,
